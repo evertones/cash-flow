01e7bf0ae3f7acb7b01289a85d40df3d6985588b
Everton Schneider <everton.schneider@gmail.com>
2018-02-17 16:24:02 +1100

    Added internationalization support.

--- src/main/java/org/evertones/CashFlowApplication.java
+++ src/main/java/org/evertones/CashFlowApplication.java
@@ -8,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class CashFlowApplication implements CommandLineRunner {
@@ -26,6 +31,22 @@ public class CashFlowApplication implements CommandLineRunner {
     @Bean
     public PasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+//        slr.setDefaultLocale(new Locale("pt", "BR"));
+//        slr.setDefaultLocale(new Locale("pt"));
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
     }
 
 	@Override
---
+++ src/main/java/org/evertones/controller/SystemController.java
@@ -0,0 +1,31 @@
+package org.evertones.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Locale;
+
+@Controller
+public class SystemController {
+
+    private MessageSource messageSource;
+
+    @Autowired
+    public void setMessageSource(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @RequestMapping(path = "/system/test", method = RequestMethod.GET)
+    public String testPage(Model model) {
+        model.addAttribute("message_en", messageSource.getMessage("message", null, Locale.US));
+        model.addAttribute("message_pt", messageSource.getMessage("message", null, new Locale("pt")));
+        return "/system/test";
+    }
+
+
+
+}
