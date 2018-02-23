f60352ead4782a9c4c6b41f4c295b0298edff3a0
Everton Schneider <everton.schneider@gmail.com>
2018-02-24 01:16:02 +1100

    Implemented edit and list page, repository and service.

--- src/main/java/org/evertones/model/product/Product.java
+++ src/main/java/org/evertones/model/product/Product.java
@@ -1,6 +1,9 @@
 package org.evertones.model.product;
 
-import org.hibernate.annotations.GeneratorType;
+import org.evertones.model.HasCreatedAt;
+import org.evertones.model.HasId;
+import org.evertones.model.HasUpdatedAt;
+import org.evertones.model.Model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,12 +12,16 @@ import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Entity
-public class Product {
+public class Product implements Model,
+        HasId<Product, Integer>,
+        HasCreatedAt<Product, LocalDate>,
+        HasUpdatedAt<Product, LocalDate>{
 
     private Integer   id;
     private String    name;
     private Double    price;
     private String    description;
+    private Boolean   active = true;
     private LocalDate createdAt;
     private LocalDate updatedAt;
 
@@ -24,48 +31,63 @@ public class Product {
         return id;
     }
 
-    public void setId(Integer id) {
+    public Product setId(Integer id) {
         this.id = id;
+        return this;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public Product setName(String name) {
         this.name = name;
+        return this;
     }
 
     public Double getPrice() {
         return price;
     }
 
-    public void setPrice(Double price) {
+    public Product setPrice(Double price) {
         this.price = price;
+        return this;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
+    public Product setDescription(String description) {
         this.description = description;
+        return this;
+    }
+
+    public Boolean isActive() {
+        return active;
+    }
+
+    public Product setActive(Boolean active) {
+        this.active = active;
+        return this;
     }
 
     public LocalDate getCreatedAt() {
         return createdAt;
     }
 
-    public void setCreatedAt(LocalDate createdAt) {
+    public Product setCreatedAt(LocalDate createdAt) {
         this.createdAt = createdAt;
+        return this;
     }
 
     public LocalDate getUpdatedAt() {
         return updatedAt;
     }
 
-    public void setUpdatedAt(LocalDate updatedAt) {
+    public Product setUpdatedAt(LocalDate updatedAt) {
         this.updatedAt = updatedAt;
+        return this;
     }
 
 }
