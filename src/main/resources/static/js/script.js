893e032034a5212e40e7c2757343e88eef77a4da
Everton Schneider <everton.schneider@gmail.com>
2018-04-15 12:16:08 +1000

    Added new features to the product UI.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
             value = value + "0";
         }
 
-        y.innerHTML = value;
+        y.innerHTML = round(value, 2);
     });
 
     /**
@@ -46,3 +46,14 @@ $(document).ready(function() {
     });
 
 });
+
+function round(number, precision) {
+    var shift = function (number, precision, reverseShift) {
+        if (reverseShift) {
+            precision = -precision;
+        }
+        numArray = ("" + number).split("e");
+        return +(numArray[0] + "e" + (numArray[1] ? (+numArray[1] + precision) : precision));
+    };
+    return shift(Math.round(shift(number, precision, false)), precision, true);
+}
